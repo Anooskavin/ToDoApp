f32f9097b0aa0d52c2e5b012c2e3e45792b68460
Anooskavin <53963293+Anooskavin@users.noreply.github.com>
2023-12-23 19:53:44 +0530

    Completed To Do

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { useState } from "react";
+import Header from "./Header/Header";
 import "./styles.css";
 
 export default function App() {
+  const [thingsToDo , setThingsToDo] = useState(["Read SpringBoot", "Complete assignments", "Prepare breakfast", "Sleep for 2 hours", "Take a shower"]);
+  const [completedToDo, setCompletedToDo] = useState([]);
+  
+
+  function enListToDo(work){
+    const updateToDo = [...completedToDo];
+    if(updateToDo.includes(work)){
+      setCompletedToDo(updateToDo.filter((value)=> value!==work));
+    }
+    else{
+      setCompletedToDo([...updateToDo,work])
+    }
+  }
+
+  function thingsToRemove(){
+    completedToDo.forEach((value)=>{
+      thingsToDo.splice(thingsToDo.indexOf(value),1);
+    })
+    setCompletedToDo([]);
+  }
+
   return (
     <div className="Application">
-      <h1>Aye Captain! 🫡</h1>
-      <h2>Lets create something!</h2>
+      <Header />
+      {thingsToDo.map((things,index)=> 
+        <p onClick={()=>enListToDo(things)} className={`listThings ${completedToDo.includes(things)? "strikeThrough" : ""}`} key={index}>
+          {things}
+        </p>
+      )}
+      {thingsToDo.length === 0 &&(
+        <h3><i>Nothing to do buddy. Sleep!</i></h3>
+      )}
+      <button onClick={thingsToRemove}>Empty</button>
     </div>
   );
 }
---
+++ src/Header/Header.js
@@ -0,0 +1,5 @@
+export default function  Headers(){
+return(
+    <h1>To-Do App</h1>
+)
+}
